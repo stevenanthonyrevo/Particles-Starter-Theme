ebaee5a3df0b02e6d2a1ac75f5026a508bcfade0
Steven <stevenanthonyrevol@live.com>
2016-04-22 22:48:29 -0400

    updated menu on mobile

---
+++ 2. Development/js/pnav.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	//button clicked once 
+    $(".p-nav-button").click(function(){
+    	//target nav menu spans and slide open/visible
+        $("#p-nav-mobile").slideToggle();
+    });
+});
